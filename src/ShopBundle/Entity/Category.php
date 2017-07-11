01298e9772f70aeaad1d9c2871688ae413c5b938
Jakub Cegielka <kuba.ceg@gmail.com>
2017-07-11 22:59:04 +0200

    category entity

---
+++ src/ShopBundle/Entity/Category.php
@@ -0,0 +1,43 @@
+<?php
+declare(strict_types=1);
+
+namespace ShopBundle\Entity;
+
+/**
+ * Category
+ */
+class Category
+{
+    /**
+     * @var int
+     */
+    private $id;
+
+    /**
+     * @var string
+     */
+    private $name;
+
+
+    public function __construct(string $name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+}
+
