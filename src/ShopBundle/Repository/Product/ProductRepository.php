8e37964ad08b73b8050cb260f03238acf895a62a
Jakub Cegielka <kuba.ceg@gmail.com>
2017-07-17 09:40:11 +0200

    Product repository

--- src/ShopBundle/Repository/Product/ProductRepository.php
+++ src/ShopBundle/Repository/Product/ProductRepository.php
@@ -6,10 +6,23 @@ namespace ShopBundle\Repository\Product;
 use Doctrine\ORM\EntityRepository;
 use ShopBundle\Entity\Product;
 
-class ProductRepository extends EntityRepository
+class ProductRepository extends EntityRepository implements ProductRepositoryInterface
 {
-    public function save(Product $product) {
+    public function save(Product $product): void
+    {
         $this->_em->persist($product);
         $this->_em->flush();
+    }
+
+    public function findByName(string $name): ?Product
+    {
+        $qb = $this->_em->createQueryBuilder()
+            ->select('p')
+            ->from("ShopBundle:Product", 'p')
+            ->where('p.name = :name');
+
+        $qb->setParameter('name', (string)$name);
+
+        return $qb->getQuery()->getOneOrNullResult();
     }
 }
